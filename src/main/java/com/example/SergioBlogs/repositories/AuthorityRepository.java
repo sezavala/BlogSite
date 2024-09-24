3e1b72f06f391d406df972bed74fdbe801dab8b5
Sergio Zavala <106635253+sezavala@users.noreply.github.com>
2024-09-24 16:12:14 -0700

    Delete src directory

--- src/main/java/com/example/SergioBlogs/repositories/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SergioBlogs.repositories;
-
-import com.example.SergioBlogs.models.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
